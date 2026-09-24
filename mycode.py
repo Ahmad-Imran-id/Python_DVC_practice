b0fd512b7af1c7307fab5e1c1447dff4a89c3cbf
Muhammad Ahmad Imran <imahmad302a@gmail.com>
2026-09-24 16:28:27 +0500

    Second version of data in dvc saved

--- mycode.py
+++ mycode.py
@@ -6,6 +6,10 @@ data={'name':['Alice','Cooper','Trevor'],
 
 df=pd.DataFrame(data)
 
+#adding new row
+new_row1 ={'name':'new1','age':34,'City':'city1'}
+df.loc[len(df.index)]=new_row1
+
 dir_name='data'
 os.makedirs(dir_name,exist_ok=True)
 
@@ -13,7 +17,6 @@ file_name=os.path.join(dir_name,'sample_data.csv')
 
 df.to_csv(file_name,index=False)
 
-print('Data saved as csv file in directory')
 
 
 
